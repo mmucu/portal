73437872dd842e8f479dd6e47975d684f08139b2
magnusotwani <magnusotwani@gmail.com>
2016-01-17 00:13:58 +0300

    Changes to database.php

--- config/database.php
+++ config/database.php
@@ -80,7 +80,7 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'     => parse_url(getenv('DATABASE_URL'))['host'],
             'database' => substr(parse_url(getenv('DATABASE_URL'))['path'],1),
             'username' => parse_url(getenv('DATABASE_URL'))['user'],
             'password' => parse_url(getenv('DATABASE_URL'))['pass'],
