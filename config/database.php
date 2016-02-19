bd97366360f265a0b7020b79896de2c753572054
magnito2 <magnusotwani@gmail.com>
2016-02-19 16:41:20 +0300

    Changes to pgsql

--- config/database.php
+++ config/database.php
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    //'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('pgsql'),
 
     /*
     |--------------------------------------------------------------------------
